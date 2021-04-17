ddc04d9d645d43f3aa4075edfd44ebcda760815a
Brian Khunchana <briankhunchana@gmail.com>
2021-04-17 01:55:52 -0500

    initial commit

---
+++ src/render.js
@@ -0,0 +1,18 @@
+// match buttons
+
+const matchOneTeamOneRoundOne = document.querySelector('#matchoneteamoneroundone');
+matchOneTeamOneRoundOne.addEventListener('click', toggleOnOff);
+
+const matchOneTeamOneRoundTwo = document.querySelector('#matchoneteamoneroundtwo');
+matchOneTeamOneRoundTwo.addEventListener('click', toggleOnOff);
+
+function toggleOnOff() {
+    if (this.style.fill === 'rgb(26, 26, 26)') {
+        this.style.fill = "rgb(255, 204, 0)";
+        this.style.opacity = '1';
+    }
+    else {
+        this.style.fill = "rgb(26, 26, 26)";
+        this.style.opacity = '.5';
+    }
+}
