865bdb03808186028a5d2b61068de48beb0aecb4
smb4122 <smb4122@gmail.com>
2021-04-17 02:32:12 -0500

    I forgot what I told ya but hoes ant shit

--- src/render.js
+++ src/render.js
@@ -6,6 +6,10 @@ matchOneTeamOneRoundOne.addEventListener('click', toggleOnOff);
 const matchOneTeamOneRoundTwo = document.querySelector('#matchoneteamoneroundtwo');
 matchOneTeamOneRoundTwo.addEventListener('click', toggleOnOff);
 
+//don't code
+//without a condom
+//that's how you get viruses
+
 function toggleOnOff() {
     if (this.style.fill === 'rgb(26, 26, 26)') {
         this.style.fill = "rgb(255, 204, 0)";
